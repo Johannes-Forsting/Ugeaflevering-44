eee06b14ffa97f6c74cc600e68216da2162fb7a8
JohannesForsting98 <johannes.forsting@gmail.com>
2021-10-27 11:53:56 +0200

    Færdig med file opggave

---
+++ src/fileReaderExercise/Main.java
@@ -0,0 +1,25 @@
+package fileReaderExercise;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> wordsInCSVFile = new ArrayList<String>();
+        File f = new File("resources/words.csv");
+        try {
+            Scanner sc = new Scanner(f);
+            while (sc.hasNextLine()){
+                String line = sc.nextLine();
+                wordsInCSVFile.add(line);
+            }
+        }
+        catch (FileNotFoundException e){
+        }
+        System.out.println(wordsInCSVFile.toString());
+        int lengthOfCSVFile = wordsInCSVFile.size();
+        System.out.println("There are " + lengthOfCSVFile + " words in the csv file");
+    }
+}
